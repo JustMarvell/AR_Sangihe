ddd433e51d47bce10d8cd9678ef9a6803e36d14e
JustMarvell <marvelmakaluwu050405@gmail.com>
2025-10-12 17:16:49 +0800

    create AudioManager Script

--- Assets/Scriptss/Audio/AudioManager.cs
+++ Assets/Scriptss/Audio/AudioManager.cs
@@ -1,6 +1,21 @@
+using FMODUnity;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    
+    public static AudioManager instance { get; private set; }
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("No Audio Manager or Found more than one Audio Manager instance detected!");
+        }
+        instance = this;
+    }
+
+    public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPos);
+    }
 }
