df36345d13cb605755296554d159d2700b786e28
JustMarvell <marvelmakaluwu050405@gmail.com>
2025-10-12 17:55:43 +0800

    update audio manager : use EventInstance

--- Assets/Scriptss/Audio/AudioManager.cs
+++ Assets/Scriptss/Audio/AudioManager.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using FMODUnity;
+using FMOD.Studio;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance { get; private set; }
+
+    private List<EventInstance> eventInstances;
 
     void Awake()
     {
@@ -12,10 +16,32 @@ public class AudioManager : MonoBehaviour
             Debug.LogError("No Audio Manager or Found more than one Audio Manager instance detected!");
         }
         instance = this;
+
+        eventInstances = new();
     }
 
     public void PlayOneShot(EventReference sound, Vector3 worldPos)
     {
         RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+
+    public EventInstance CreateEventInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        eventInstances.Add(eventInstance);
+        return eventInstance;
+    }
+
+    private void CleanUp()
+    {
+        foreach (EventInstance eventInstance in eventInstances)
+        {
+            eventInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            eventInstance.release();
+        }
+    }
+    
+    private void OnDestroy() {
+        CleanUp();
     }
 }
